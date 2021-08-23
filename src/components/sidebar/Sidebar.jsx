58c061f51f17be793eea4fca460774834bbd681d
Vin_1999 <vinothinivijayakumaran08@gmail.com>
2021-08-23 18:07:11 +0100

    initial commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './sidebar.css'
+import logo from '../../assets/images/logo.png'
+import sidebar_items from '../../assets/JsonData/sidebar_routes.json'
+const Sidebar = () => {
+  return (
+    <div>
+       Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
